5a9abe0e69f16413c6be2b7013a7a14c34809eaf
Abdulrasheed <yinka.jsx@gmail.com>
2024-10-19 05:38:04 +0100

    Refactor flow-footer component to hide on small screens

--- components/layout/flow-footer.tsx
+++ components/layout/flow-footer.tsx
@@ -16,7 +16,7 @@ export function FlowFooter({
       gap={15}
       wrap='wrap'
       hidden={!visible}
-      className='lg:justify-between flex-col lg:flex-row lg:items-center border-t border-primary-border-light rounded-b-4 bg-primary-background-white sm:px-12'
+      className='lg:justify-between hidden sm:flex flex-col lg:flex-row lg:items-center border-t border-primary-border-light rounded-b-4 bg-primary-background-white sm:px-12'
       {...props}
     >
       {children}
