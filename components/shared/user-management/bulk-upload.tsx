74739dfa0386575f15c3be3f3f9e1344f67cc593
Abdulrasheed <yinka.jsx@gmail.com>
2024-11-28 13:04:28 +0100

    Refactor code: Add bulk upload functionality for occupants

--- components/shared/user-management/bulk-upload.tsx
+++ components/shared/user-management/bulk-upload.tsx
@@ -1,18 +1,27 @@
 "use client";
 
-import { Box, Button, Flex, Text } from "@mantine/core";
+import { Box, Button, Flex, Stack, Text } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { useQueryClient } from "@tanstack/react-query";
-import { Fragment, ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
-import { builder } from "@/builders";
 import { MODALS } from "@/packages/libraries";
 import { handleError, handleSuccess } from "@/packages/notification";
 import { ResourceUpload } from "../uploads/resource";
 import { FILE } from "@/packages/libraries/enum";
 import Link from "next/link";
 import { DownloadIcon } from "@/icons";
-import { useFileUpload } from "@/packages/hooks/use-file-upload";
+import { Files, useFileUpload } from "@/packages/hooks/use-file-upload";
+
+import Papa from "papaparse";
+import { occupants } from "@/builders/http/occupants";
+import { occupantsColumns } from "@/columns/for_admins/occupants";
+import {
+  FlowContentContainer,
+  FlowPaper,
+  FlowStateProvider,
+  FlowTable,
+} from "@/components/layout";
 
 type StaffListUploadProps = {
   organization_id: number;
@@ -20,14 +29,48 @@ type StaffListUploadProps = {
 };
 
 export function BulkUpload(props: StaffListUploadProps) {
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [parsedData, setParsedData] = useState<any[]>([]);
+  const [status, setStatus] = useState<
+    "pending" | "uploaded" | "uploading" | "dropped" | "error" | "waiting"
+  >("pending");
+
   const queryClient = useQueryClient();
 
   function handleClose() {
     modals.close(MODALS.UPLOAD_RESOURCES);
   }
 
+  const requiredFields = ["name", "email", "age"];
+
+  function handleParsing([file]: Files) {
+    if (!file) return;
+
+    setUploadedFile(file);
+    Papa.parse(file, {
+      header: true,
+
+      complete: (result) => {
+        const missingFields = requiredFields.filter(
+          (field) => !result.meta.fields?.includes(field)
+        );
+
+        if (missingFields.length > 0) {
+          return handleError({
+            message: `The following fields are required: ${missingFields.join(
+              ", "
+            )}`,
+          })();
+        }
+
+        setStatus("waiting");
+        setParsedData(result.data);
+      },
+    });
+  }
+
   const {
-    status,
+    status: uploadStatus,
     preview,
     progress,
     isPending,
@@ -56,57 +99,64 @@ export function BulkUpload(props: StaffListUploadProps) {
         classNames={{
           label: "flex items-center justify-between w-full text-primary-body",
         }}
+        component={Link}
+        href='https://res.cloudinary.com/techeducratic/raw/upload/v1721248216/uploads/2024/07/17/igv6lzhidxz3e5am8erp.csv'
       >
-        <Text
-          component={Link}
-          href='https://res.cloudinary.com/techeducratic/raw/upload/v1721248216/uploads/2024/07/17/igv6lzhidxz3e5am8erp.csv'
-        >
-          Download file format
-        </Text>
+        <Text>Download file format</Text>
         <DownloadIcon color='var(--accent-7)' className='ml-auto' />
       </Button>
     ),
     uploaded: "Uploaded successfully",
     uploading: "Uploading...",
-    dropped: "Awaiting upload...",
+    await: "Awaiting upload...",
     error: "An error occurred. Please try again",
+    dropped: (
+      <FlowContentContainer
+        mah={300}
+        className='rounded-none lg:rounded-2xl bg-white w-full'
+      >
+        <FlowTable data={parsedData} columns={occupantsColumns} />
+      </FlowContentContainer>
+    ),
   };
 
   return (
-    <Box component='form' onSubmit={handleSubmit(handleUpload)}>
-      <Text className='text-primary-text-subtle' mb={15}>
-        {view[status]}
-      </Text>
-      <ResourceUpload
-        accepts={(mime) => [mime.csv]}
-        onDrop={handleDrop}
-        name={preview.name}
-        size={preview.size}
-        completed={progress?.completed}
-        status={status}
-        supports={["csv", "xls"]}
-        multiple={false}
-      />
+    <FlowStateProvider>
+      <Box component='form' onSubmit={handleSubmit(handleUpload)}>
+        <Text className='text-primary-text-subtle' mb={15}>
+          {view[status]}
+        </Text>
+        <ResourceUpload
+          accepts={(mime) => [mime.csv]}
+          onDrop={handleParsing}
+          name={preview.name}
+          size={preview.size}
+          completed={progress?.completed}
+          status={uploadStatus}
+          supports={["csv", "xls"]}
+          multiple={false}
+        />
 
-      <Flex gap={12} wrap='wrap' mt={30}>
-        <Button
-          flex={1}
-          miw='fit-content'
-          variant='default'
-          onClick={handleClose}
-        >
-          Cancel
-        </Button>
-        <Button
-          flex={1}
-          miw='fit-content'
-          type='submit'
-          disabled={isPending || status === "pending"}
-          loading={isPending}
-        >
-          Confirm
-        </Button>
-      </Flex>
-    </Box>
+        <Flex gap={12} wrap='wrap' mt={30}>
+          <Button
+            flex={1}
+            miw='fit-content'
+            variant='default'
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            flex={1}
+            miw='fit-content'
+            type='submit'
+            disabled={isPending || status === "pending"}
+            loading={isPending}
+          >
+            Confirm
+          </Button>
+        </Flex>
+      </Box>
+    </FlowStateProvider>
   );
 }
