ec7649c7d449d3e245f5aa25e7ba03c3503f3af3
Abdulrasheed <yinka.jsx@gmail.com>
2025-01-02 05:27:05 +0100

    Refactor: Update button labels and styles in market-place page and listings component

--- app/(users)/@admin/dashboard/market-place/page.tsx
+++ app/(users)/@admin/dashboard/market-place/page.tsx
@@ -24,11 +24,11 @@ import { useQuery } from "@tanstack/react-query";
 import { useQueryState } from "nuqs";
 import { Fragment, useEffect } from "react";
 
+import { FilterDropdown } from "@/components/shared/interface/dropdowns";
 import { PRODUCT_CATEGORIES, PRODUCT_VIEW } from "@/packages/constants/data";
 import clsx from "clsx";
 import { getCookie } from "cookies-next";
 import { toString } from "lodash";
-import { FilterDropdown } from "@/components/shared/interface/dropdowns";
 
 const filterOptions = [
   { label: "Recent", value: "Recent" },
@@ -111,7 +111,9 @@ export default function MarketPlace() {
       <FlowContainer type='plain' className='lg:~p-1/8'>
         <FlowContentContainer
           classNames={{
-            root: "rounded-none lg:rounded-2xl bg-white",
+            root: clsx("rounded-none lg:rounded-2xl", {
+              "bg-white": noDataAvailable || isPlaceholderData,
+            }),
           }}
         >
           <FlowTabs
